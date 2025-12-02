374de884d6683a00e722e8edd7d048a2b3a33a38
Heikki Sateila <heikki@sateila.net>
2025-12-02 22:34:57 +0200

    Cypress test -esimerkit valmiina

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:5173',
+    specPattern: 'tests/cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
+    supportFile: 'tests/cypress/support/e2e.js',
+    fixturesFolder: 'tests/cypress/fixtures',
   },
 });
