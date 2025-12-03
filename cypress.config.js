c8fa8b7a74dd44a5f24a07c205ccf6ca4425cb6b
Heikki Sateila <heikki@sateila.net>
2025-12-03 18:35:49 +0200

    Cypress-konfiguraation kommentteja

--- cypress.config.js
+++ cypress.config.js
@@ -5,9 +5,11 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:5173',
-    specPattern: 'tests/cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    supportFile: 'tests/cypress/support/e2e.js',
-    fixturesFolder: 'tests/cypress/fixtures',
+
+    // Cypress-konfiguraatiot
+    baseUrl: 'http://localhost:5173', // sovelluksen sijaintiurl kehitykympäristössä
+    specPattern: 'tests/cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}', // polku ja tiedostonimimuoto, jolla Cypress etsii testitiedostot
+    supportFile: 'tests/cypress/support/e2e.js', // polku ja tiedostonimimuoto, jolla Cypress etsii supportFilen
+    fixturesFolder: 'tests/cypress/fixtures', // polku ja tiedostonimimuoto, jolla Cypress etsii fixtures-kansion
   },
 });
